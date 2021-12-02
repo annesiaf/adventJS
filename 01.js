7d9583b93e54c5c6454fcf024ee83dff80212e80
Brahianpdev <zkspywn@gmail.com>
2021-12-02 11:32:25 -0300

    fixed

--- 01.js
+++ 01.js
@@ -21,41 +21,46 @@ console.log(ovejasFiltradas);
 
 // Resolucion con razonamiento propio:
 
+// 0. Razonamiento:
+// 1. Filtrar la cantidad de ovejas que sean de color rojo
+// 2. Filtrar las ovejas que tengan n && a en su nombre
+// 3. Optimizar codigo
+
+// Procedimiento:
+// 1. Hice la task 1 & 2 por separadas.
+// 2. Optimice codigo
+
+// Filtrar ovejas color roja
+const ovejasRojas = ovejas.filter((roja) => roja.color === "rojo");
+console.log(ovejasRojas);
+
+// Filtrar por n & a (Pense en recorrer el arreglo, convirtiendo todos los names toLowerCase, pero...)
+const haveLetter = ovejas.filter((letter) =>
+  letter.name.includes("a", "n", "A", "N")
+);
+console.log(haveLetter);
+
+// Optimizacion de codigo:
+let ovejasConAN = ovejas.filter(
+  (result) => result.name.includes("a", "A", "n", "N") && result.color == "rojo"
+);
+
+console.log(ovejasConAN);
+
+// El test del ejercicio me obliga a usar toLowerCase, asi que, refactoring time! ...
+ovejas.filter(
+  (result) =>
+    result.name.toLowerCase().includes("a" && "n") && result.color == "rojo"
+);
+// F5: Hmmm, en mi codigo anda, pero en la pagina... "Algunos test fallaron" !??
+// Seguro me esta faltando algo por validar :D o, quizas...
+
+// Dos doritos despues... Resolucion final:
 export default function contarOvejas(ovejas) {
-  // 0. Razonamiento:
-  // 1. Filtrar la cantidad de ovejas que sean de color rojo
-  // 2. Filtrar las ovejas que tengan n && a en su nombre
-  // 3. Optimizar codigo
-
-  // Procedimiento:
-  // 1. Hice la task 1 & 2 por separadas.
-  // 2. Optimice codigo
-
-  // Filtrar ovejas color roja
-    const ovejasRojas = ovejas.filter((roja) => roja.color === "rojo");
-    console.log(ovejasRojas);
-
-  // Filtrar por n & a (Pense en recorrer el arreglo, convirtiendo todos los names toLowerCase, pero...)
-    const haveLetter = ovejas.filter((letter) => letter.name.includes("a", "n", "A", "N"));
-    console.log(haveLetter);
-
-  // Optimizacion de codigo: 
-    let ovejasConAN = ovejas.filter(
-        result => ((result.name).includes('a', 'A', 'n', 'N')
-          && result.color == 'rojo'));
-
-    console.log(ovejasConAN);
-
-   // El test del ejercicio me obliga a usar toLowerCase, asi que, refactoring time! ... 
-   ovejas.filter(result => (((result.name.toLowerCase())).includes('a' && 'n') && result.color == 'rojo'));
-   // F5: Hmmm, en mi codigo anda, pero en la pagina... "Algunos test fallaron" !?? 
-   // Seguro me esta faltando algo por validar :D o, quizas...
-
-   // Dos doritos despues... Resolucion final:
   const ovejitas = ovejas.filter((oveja) => {
-  oveja.name.toLowerCase();
-  if (name.includes('a', 'n') && oveja.color == 'rojo') return oveja
-  })
+    oveja.name.toLowerCase();
+    if (name.includes("a", "n") && oveja.color == "rojo") return oveja;
+  });
 
-return ovejitas
+  return ovejitas;
 }
